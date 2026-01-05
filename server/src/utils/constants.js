733ee50f7f0ec85f1c30671ff90cb7d513f48907
ChaoBanhNguyen <102232031+Chaobanh27@users.noreply.github.com>
2026-01-06 00:25:28 +0700

    fix whitelist-domains

--- server/src/utils/constants.js
+++ server/src/utils/constants.js
@@ -2,8 +2,8 @@ import { env } from '~/config/environment'
 
 //nhung domain duoc phep truy cap toi tai nguyen server
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173',
-  'https://mern-blog-main-client.vercel.app/'
+  'https://mern-blog-main-client.vercel.app',
+  'http://localhost:5173'
 ]
 
 export const WEBSITE_DOMAIN = (env.BUILD_MODE === 'production') ? env.WEBSITE_DOMAIN_PRODUCTION : env.WEBSITE_DOMAIN_DEVELOPMENT
